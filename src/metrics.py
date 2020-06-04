b2989820b2370c679f8f2beffc93d23c6ce20690
barinsm <36234030+barinsm@users.noreply.github.com>
2020-06-04 17:22:58 +0300

    Add files via upload

---
+++ src/metrics.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def precision_at_k(recommended_list, bought_list, k=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    bought_list = bought_list  # Тут нет [:k] !!
+    recommended_list = recommended_list[:k]
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    precision = flags.sum() / len(recommended_list)
+    
+    return precision
+
+
+def recall_at_k(recommended_list, bought_list, k=5):
+    
+    # your_code
+    
+    return recall
+
+
+# In[ ]:
+
+
+
+
